4c33f69a7704903efbfedd3f160cd0652b2ffbf2
es <StaNC2000>
2020-01-25 14:16:52 +0100

    reverseNumber method completed

--- src/com/eS/NumberToWords.java
+++ src/com/eS/NumberToWords.java
@@ -7,8 +7,17 @@ public class NumberToWords {
     }
 
     public static int reverseNumber(int number){
+        int reversingNumber = number;
+        int reminder;
+        int reverse = 0;
 
-        return - 1;
+        while (reversingNumber != 0){
+            reminder = reversingNumber % 10;
+            reverse = (reverse * 10) + reminder;
+            reversingNumber = reversingNumber / 10;
+        }
+
+        return reverse;
     }
 
 
@@ -24,13 +33,13 @@ public class NumberToWords {
                 digit = digit / 10;
             }
         }
-        return counter;
+        return counter + 1;
     }
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println(getDigitCount(656465));
-
+//        System.out.println(getDigitCount(102));
+        System.out.println(reverseNumber(12));
     }
 }
