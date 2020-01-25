f83cc21a543a6d9f40478fe33c79ff4e9c09c0c9
es <StaNC2000>
2020-01-25 12:56:56 +0100

    getDigitCount methode added, method are checking for lenght of parameter

--- src/com/eS/NumberToWords.java
+++ src/com/eS/NumberToWords.java
@@ -7,14 +7,24 @@ public class NumberToWords {
     }
 
     public static int getDigitCount(int number){
-        if (number < 0){
+        int counter;
+        int digit = number;
+        if (number <= 0){
             return - 1;
         }
 
-        return number;
+        for (counter = 0; counter <= digit; counter++){
+            if (digit <= number){
+                digit = digit / 10;
+            }
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println(getDigitCount(656465));
+
     }
 }
