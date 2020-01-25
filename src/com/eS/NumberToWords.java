17dd6ae08adda7201bb2da331c85af6a71bd710f
es <StaNC2000>
2020-01-25 12:19:25 +0100

    Initial commit for getDigitCount

--- src/com/eS/NumberToWords.java
+++ src/com/eS/NumberToWords.java
@@ -2,6 +2,18 @@ package com.eS;
 
 public class NumberToWords {
 
+    public static void numberToWords(int number){
+
+    }
+
+    public static int getDigitCount(int number){
+        if (number < 0){
+            return - 1;
+        }
+
+        return number;
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
